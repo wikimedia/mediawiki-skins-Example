74420c627a9edd2fc061e482a325fccaff2b5d71
Isarra <zhorishna@gmail.com>
2017-05-19 21:35:06 +0000

    Refactor example skin to use single echo statement
    Change-Id: I9d92ddf2011a2904f4c9aaee4a3286e0b21877ad

--- ExampleTemplate.php
+++ ExampleTemplate.php
@@ -9,117 +9,78 @@ class ExampleTemplate extends BaseTemplate {
 	 * Outputs the entire contents of the page
 	 */
 	public function execute() {
-		$this->html( 'headelement' );
-		?>
-		<div id="mw-wrapper">
-			<div class="mw-body" role="main">
-				<?php
-				if ( $this->data['sitenotice'] ) {
-					echo Html::rawElement(
-						'div',
-						array( 'id' => 'siteNotice' ),
-						$this->get( 'sitenotice' )
-					);
-				}
-				if ( $this->data['newtalk'] ) {
-					echo Html::rawElement(
-						'div',
-						array( 'class' => 'usermessage' ),
-						$this->get( 'newtalk' )
-					);
-				}
-				echo $this->getIndicators();
-				echo Html::rawElement(
-					'h1',
-					array(
+		$html = '';
+		$html .= $this->get( 'headelement' );
+
+		$html .= Html::rawElement( 'div', [ 'id' => 'mw-wrapper' ],
+			Html::rawElement( 'div', [ 'class' => 'mw-body', 'role' => 'main' ],
+				$this->getSiteNotice() .
+				$this->getNewTalk() .
+				$this->getIndicators() .
+				Html::rawElement( 'h1',
+					[
 						'class' => 'firstHeading',
 						'lang' => $this->get( 'pageLanguage' )
-					),
+					],
 					$this->get( 'title' )
-				);
-
-				echo Html::rawElement(
-					'div',
-					array( 'id' => 'siteSub' ),
+				) .
+				Html::rawElement( 'div', [ 'id' => 'siteSub' ],
 					$this->getMsg( 'tagline' )->parse()
-				);
-				?>
-
-				<div class="mw-body-content">
-					<?php
-					echo Html::openElement(
-						'div',
-						array( 'id' => 'contentSub' )
-					);
-					if ( $this->data['subtitle'] ) {
-						echo Html::rawelement (
+				) .
+				Html::rawElement( 'div', [ 'class' => 'mw-body-content' ],
+					Html::rawElement( 'div', [ 'id' => 'contentSub' ],
+						$this->getPageSubtitle() .
+						Html::rawelement (
 							'p',
 							[],
-							$this->get( 'subtitle' )
-						);
-					}
-					echo Html::rawelement (
-						'p',
-						[],
-						$this->get( 'undelete' )
-					);
-					echo Html::closeElement( 'div' );
-
-					$this->html( 'bodycontent' );
-					echo $this->getClear();
-					echo Html::rawElement(
-						'div',
-						array( 'class' => 'printfooter' ),
+							$this->get( 'undelete' )
+						)
+					) .
+					$this->get( 'bodycontent' ) .
+					$this->getClear() .
+					Html::rawElement( 'div', [ 'class' => 'printfooter' ],
 						$this->get( 'printfooter' )
-					);
-					$this->html( 'catlinks' );
-					$this->html( 'dataAfterContent' );
-					?>
-				</div>
-			</div>
-
-			<div id="mw-navigation">
-				<?php
-				echo Html::rawElement(
+					) .
+					$this->getCategoryLinks() .
+					$this->getDataAfterContent() .
+					$this->get( 'debughtml' )
+				)
+			) .
+			Html::rawElement( 'div', [ 'id' => 'mw-navigation' ],
+				Html::rawElement(
 					'h2',
 					[],
 					$this->getMsg( 'navigation-heading' )->parse()
-				);
-
-				echo $this->getLogo();
-				echo $this->getSearch();
-
+				) .
+				$this->getLogo() .
+				$this->getSearch() .
 				// User profile links
-				echo Html::rawElement(
+				Html::rawElement(
 					'div',
-					array( 'id' => 'user-tools' ),
+					[ 'id' => 'user-tools' ],
 					$this->getUserLinks()
-				);
-
+				) .
 				// Page editing and tools
-				echo Html::rawElement(
+				Html::rawElement(
 					'div',
-					array( 'id' => 'page-tools' ),
+					[ 'id' => 'page-tools' ],
 					$this->getPageLinks()
-				);
-
+				) .
 				// Site navigation/sidebar
-				echo Html::rawElement(
+				Html::rawElement(
 					'div',
-					array( 'id' => 'site-navigation' ),
+					[ 'id' => 'site-navigation' ],
 					$this->getSiteNavigation()
-				);
-				?>
-			</div>
+				)
+			) .
+			$this->getFooter()
+		);
 
-			<?php echo $this->getFooter(); ?>
-		</div>
+		$html .= $this->getTrail();
+		$html .= Html::closeElement( 'body' );
+		$html .= Html::closeElement( 'html' );
 
-		<?php $this->printTrail() ?>
-		</body>
-		</html>
-
-		<?php
+		echo $html;
 	}
 
 	/**
@@ -129,27 +90,27 @@ class ExampleTemplate extends BaseTemplate {
 	protected function getLogo( $id = 'p-logo', $imageOnly = false ) {
 		$html = Html::openElement(
 			'div',
-			array(
+			[
 				'id' => $id,
 				'class' => 'mw-portlet',
 				'role' => 'banner'
-			)
+			]
 		);
 		$html .= Html::element(
 			'a',
-			array(
+			[
 				'href' => $this->data['nav_urls']['mainpage']['href'],
 				'class' => 'mw-wiki-logo',
-			) + Linker::tooltipAndAccesskeyAttribs( 'p-logo' )
+			] + Linker::tooltipAndAccesskeyAttribs( 'p-logo' )
 		);
 		if ( !$imageOnly ) {
 			$html .= Html::element(
 				'a',
-				array(
+				[
 					'id' => 'p-banner',
 					'class' => 'mw-wiki-title',
 					'href'=> $this->data['nav_urls']['mainpage']['href']
-				) + Linker::tooltipAndAccesskeyAttribs( 'p-logo' ),
+				] + Linker::tooltipAndAccesskeyAttribs( 'p-logo' ),
 				$this->getMsg( 'sitetitle' )->escaped()
 			);
 		}
@@ -165,12 +126,12 @@ class ExampleTemplate extends BaseTemplate {
 	protected function getSearch() {
 		$html = Html::openElement(
 			'form',
-			array(
+			[
 				'action' => htmlspecialchars( $this->get( 'wgScript' ) ),
 				'role' => 'search',
 				'class' => 'mw-portlet',
 				'id' => 'p-search'
-			)
+			]
 		);
 		$html .= Html::hidden( 'title', htmlspecialchars( $this->get( 'searchtitle' ) ) );
 		$html .= Html::rawelement(
@@ -178,8 +139,8 @@ class ExampleTemplate extends BaseTemplate {
 			[],
 			Html::label( $this->getMsg( 'search' )->escaped(), 'searchInput' )
 		);
-		$html .= $this->makeSearchInput( array( 'id' => 'searchInput' ) );
-		$html .= $this->makeSearchButton( 'go', array( 'id' => 'searchGoButton', 'class' => 'searchButton' ) );
+		$html .= $this->makeSearchInput( [ 'id' => 'searchInput' ] );
+		$html .= $this->makeSearchButton( 'go', [ 'id' => 'searchGoButton', 'class' => 'searchButton' ] );
 		$html .= Html::closeElement( 'form' );
 
 		return $html;
@@ -272,6 +233,74 @@ class ExampleTemplate extends BaseTemplate {
 			$this->getPersonalTools(),
 			'personaltools'
 		);
+	}
+
+	/**
+	 * Generates siteNotice, if any
+	 * @return string html
+	 */
+	protected function getSiteNotice() {
+		if ( $this->data['sitenotice'] ) {
+			return Html::rawElement(
+				'div',
+				[ 'id' => 'siteNotice' ],
+				$this->get( 'sitenotice' )
+			);
+		}
+		return '';
+	}
+
+	/**
+	 * Generates new talk message banner, if any
+	 * @return string html
+	 */
+	protected function getNewTalk() {
+		if ( $this->data['newtalk'] ) {
+			return Html::rawElement(
+				'div',
+				[ 'class' => 'usermessage' ],
+				$this->get( 'newtalk' )
+			);
+		}
+		return '';
+	}
+
+	/**
+	 * Generates subtitle stuff, if any
+	 * @return string html
+	 */
+	protected function getPageSubtitle() {
+		if ( $this->data['subtitle'] ) {
+			return Html::rawelement (
+				'p',
+				[],
+				$this->get( 'subtitle' )
+			);
+		}
+		return '';
+	}
+
+
+	/**
+	 * Generates category links, if any
+	 * @return string html
+	 */
+	protected function getCategoryLinks() {
+		if ( $this->data['catlinks'] ) {
+			return $this->get( 'catlinks' );
+		}
+		return '';
+	}
+
+	/**
+	 * Generates data after content stuff, if any
+	 * @return string html
+	 */
+	protected function getDataAfterContent() {
+		if ( $this->data['dataAfterContent'] ) {
+			return $this->get( 'dataAfterContent' );
+		}
+		return '';
 	}
 
 	/**
@@ -448,6 +477,22 @@ class ExampleTemplate extends BaseTemplate {
 				$content
 			);
 		}
+
+		return $html;
+	}
+
+	/**
+	 * Get the basic end-page trail including bottomscripts, reporttime, and
+	 * debug stuff. This should be called right before outputting the closing
+	 * body and html tags.
+	 *
+	 * @return string
+	 * @since 1.29
+	 */
+	function getTrail() {
+		$html = MWDebug::getDebugHTML( $this->getSkin()->getContext() );
+		$html .= $this->get( 'bottomscripts' );
+		$html .= $this->get( 'reporttime' );
 
 		return $html;
 	}
