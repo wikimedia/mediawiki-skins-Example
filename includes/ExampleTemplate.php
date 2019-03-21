479a18546551fb0c4a3578acb001bc5bc779e9ec
Kunal Mehta <legoktm@member.fsf.org>
2019-03-20 19:30:45 -0700

    Upgrade to newer phan
    Change-Id: I2af5cbde7ee658bb052b8f4f1329de37744641c2

--- includes/ExampleTemplate.php
+++ includes/ExampleTemplate.php
@@ -454,6 +454,7 @@ class ExampleTemplate extends BaseTemplate {
 			}
 			// Compatibility with extensions still using SkinTemplateToolboxEnd or similar
 			if ( is_array( $options['hooks'] ) ) {
+				// @phan-suppress-next-line PhanTypeMismatchForeach T218843
 				foreach ( $options['hooks'] as $hook ) {
 					if ( is_string( $hook ) ) {
 						$hookOptions = [];
