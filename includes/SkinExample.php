3dbb124dbb3dfe46e0bfdc507fef6afb32166988
jdlrobson <jdlrobson@gmail.com>
2020-09-04 09:00:16 -0700

    Simplify SkinExample
    * Switch example skin to skin options as skin
    options can now be used to list skin styles and scripts.
    * stylename only needs to be set if using getSkinStylePath - most
    skin developers shouldn't need to know about this
    * $skinname is set via options now
    
    Change-Id: Ie2fd749a0e2c9f16c79c7fba4e26d32ef2aa7831

--- includes/SkinExample.php
+++ includes/SkinExample.php
@@ -8,30 +8,4 @@ class SkinExample extends SkinTemplate {
 	public $skinname = 'example',
 		$stylename = 'Example',
 		$template = 'ExampleTemplate';
-
-	/**
-	 * Add CSS via ResourceLoader
-	 *
-	 * @param OutputPage $out
-	 */
-	public function initPage( OutputPage $out ) {
-		$out->addMeta( 'viewport',
-			'width=device-width, initial-scale=1.0, ' .
-			'user-scalable=yes, minimum-scale=0.25, maximum-scale=5.0'
-		);
-
-		$out->addModuleStyles( [
-			'skins.example'
-		] );
-		$out->addModules( [
-			'skins.example.js'
-		] );
-	}
-
-	/**
-	 * @param OutputPage $out
-	 */
-	function setupSkinUserCss( OutputPage $out ) {
-		parent::setupSkinUserCss( $out );
-	}
 }
